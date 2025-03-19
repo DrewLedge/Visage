e6aa49c0eccb455aaa5579af3d559a16124b8100
Drew <83474944+DrewLedge@users.noreply.github.com>
2025-03-19 19:46:12 -0400

    lighting updates

--- engine/shaders/includes/lightingcalc.glsl
+++ engine/shaders/includes/lightingcalc.glsl
@@ -1,32 +1,37 @@
-float getAttenuation(int lightIndex, int frame, vec3 lightPos, vec3 fragPos) {
-    float constAttenuation = lssbo[frame].lights[lightIndex].constantAttenuation;
-    float linAttenuation = lssbo[frame].lights[lightIndex].linearAttenuation;
-    float quadAttenuation = lssbo[frame].lights[lightIndex].quadraticAttenuation;
-    float lightDistance = distance(lightPos, fragPos);
+vec3 spotlightEmittedRadience(LightData light, vec3 pos, vec3 lightPos, vec3 fragLightDir) {
+    vec3 lightColor = light.color.xyz;
+    vec3 target = light.target.xyz;
 
-    return 1.0f / (constAttenuation + linAttenuation * lightDistance + quadAttenuation * (lightDistance * lightDistance));
-}
+    float intensity = light.intensity;
 
-float getLightConeFactor(int lightIndex, int frame, vec3 fragLightDir, vec3 lightPos) {
-    float inner = lssbo[frame].lights[lightIndex].innerConeAngle;
-    float outer = lssbo[frame].lights[lightIndex].outerConeAngle;
+    float inner = light.innerConeAngle;
+    float outer = light.outerConeAngle;
 
-    vec3 target = lssbo[frame].lights[lightIndex].target.xyz;
-    vec3 spotDir = normalize(lightPos - target);
+    float constAttenuation = light.constantAttenuation;
+    float linAttenuation = light.linearAttenuation;
+    float quadAttenuation = light.quadraticAttenuation;
 
-    float theta = dot(spotDir, fragLightDir);
+    // light falloff
+    vec3 spotDir = normalize(target - lightPos);
+    float theta = dot(spotDir, -fragLightDir);
 
-    if (theta <= cos(outer)) {
-        return 0.0f;
+    if (theta < cos(outer)) {
+        return vec3(0.0f);
     }
 
-    float factor = smoothstep(cos(outer), cos(inner), theta);
-    return factor * factor;
+    float falloff = smoothstep(cos(outer), cos(inner), theta);
+    falloff *= falloff;
+
+    // light attenuation
+    float lightDistance = distance(lightPos, pos);
+    float attenuation = 1.0f / (constAttenuation + linAttenuation * lightDistance + quadAttenuation * (lightDistance * lightDistance));
+
+    return lightColor * intensity * falloff * attenuation;
 }
 
-float getShadowFactor(int lightIndex, int frame, vec3 fragPos, int frameCount, int lightsPerBatch) {
+float getShadowFactor(LightData light, int lightIndex, int frame, vec3 fragPos, int frameCount, int lightsPerBatch) {
     // get the frag pos in light space
-    vec4 fragPosLightspace = lssbo[frame].lights[lightIndex].vp * vec4(fragPos, 1.0f);
+    vec4 fragPosLightspace = light.vp * vec4(fragPos, 1.0f);
 
     // perspective divide and transfrom x and y compoenent to 0 - 1 range
     // this is because the z component is already in the 0 - 1 range
@@ -52,26 +57,21 @@ vec4 calcLighting(vec4 albedo, vec4 metallicRoughness, vec3 normal, vec3 emissiv
     float metallic = metallicRoughness.b;
 
     for (int i = 0; i < lightCount; i++) {
-        if (lssbo[frame].lights[i].intensity < 0.01f) continue;
+        LightData light = lssbo[frame].lights[i];
 
-        vec3 lightPos = lssbo[frame].lights[i].pos.xyz;
-        vec3 lightColor = lssbo[frame].lights[i].color.xyz;
+        if (light.intensity < 0.01f) continue;
+
+        vec3 lightPos = light.pos.xyz;
         vec3 fragLightDir = normalize(lightPos - fragPos);
 
-        float lightConeFactor = getLightConeFactor(i, frame, fragLightDir, lightPos);
-        if (lightConeFactor < 0.01f) continue;
+        vec3 Le = spotlightEmittedRadience(light, fragPos, lightPos, fragLightDir);
+        if (length(Le) < 0.05f) continue;
 
-        float shadowFactor = getShadowFactor(i, frame, fragPos, frameCount, lightsPerBatch);
-        if (shadowFactor < 0.04f) continue;
-
-        float attenuation = getAttenuation(i, frame, lightPos, fragPos);
-        if (attenuation < 0.01f) continue;
-
-        float contribution = lssbo[frame].lights[i].intensity * attenuation * lightConeFactor;
-        if (contribution < 0.01f) continue;
+        float shadowFactor = getShadowFactor(light, i, frame, fragPos, frameCount, lightsPerBatch);
+        if (shadowFactor < 0.05f) continue;
 
         vec3 brdf = cookTorrance(normal, fragLightDir, viewDir, albedo, metallic, roughness);
-        accumulated += (brdf * lightColor * contribution * shadowFactor);
+        accumulated += (brdf * Le * shadowFactor);
     }
 
     // final color calculation
